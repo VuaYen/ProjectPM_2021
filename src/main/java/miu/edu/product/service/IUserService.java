48558f4e442aaebb0cc79dce85e614a52250460a
VuaYen <yen637@gmail.com>
2021-05-10 13:22:47 -0700

    update  code register UI

---
+++ src/main/java/miu/edu/product/service/IUserService.java
@@ -0,0 +1,17 @@
+package miu.edu.product.service;
+
+
+import miu.edu.product.domain.User;
+
+import java.util.List;
+
+public interface IUserService {
+    public  boolean createUser(User user);
+    public <T extends User> T saveProfile(T user);
+
+    public List<User> getNewVendorUser();
+
+    public void approveNewUser(boolean status, String username);
+
+    public <T extends User> T getByUsername(String username);
+}
