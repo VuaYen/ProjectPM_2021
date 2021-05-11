aac5d8b82d15eb2f025e77a10c1167b7af19d1c2
edfortune <edmael.fortunex@gmail.com>
2021-05-10 19:49:03 -0500

    Add user service

--- src/main/java/miu/edu/product/service/UserService.java
+++ src/main/java/miu/edu/product/service/UserService.java
@@ -4,6 +4,7 @@ import miu.edu.product.domain.User;
 import miu.edu.product.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,7 +17,11 @@ public class UserService implements IUserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User save(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
