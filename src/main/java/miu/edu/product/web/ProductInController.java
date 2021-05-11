51e05e69118863559c828207c7018186807c807f
VuaYen <yen637@gmail.com>
2021-05-11 14:18:05 -0700

    Update the link order

--- src/main/java/miu/edu/product/web/ProductInController.java
+++ src/main/java/miu/edu/product/web/ProductInController.java
@@ -1,16 +1,17 @@
 package miu.edu.product.web;
 
 import miu.edu.product.domain.ApiResponse;
+import miu.edu.product.domain.OrderStatus;
 import miu.edu.product.domain.Product;
 import miu.edu.product.domain.Vendor;
 import miu.edu.product.dto.ReportRequestDTO;
 import miu.edu.product.dto.ReportResponseDTO;
 import miu.edu.product.repository.VendorRepository;
-import miu.edu.product.service.CategoryService;
-import miu.edu.product.service.Converter;
-import miu.edu.product.service.ProductService;
-import miu.edu.product.service.ReportService;
+import miu.edu.product.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.support.SessionStatus;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -42,6 +44,13 @@ public class ProductInController {
 
     @Autowired
     ReportService reportService;
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderDetailService orderDetailService;
+
     @GetMapping("/report")
     public String generateDataReport(HttpServletRequest httpServletRequest,Model model) {
         ApiResponse<ReportResponseDTO> response = new ApiResponse<>();
@@ -65,6 +74,30 @@ public class ProductInController {
 
         model.addAttribute("reports", response.getData());
         return "admin/report";
+    }
+
+    @GetMapping("approve")
+    public String productsregister(Model model, HttpServletRequest request){
+
+        List<Product> productOptional = productService.all();
+
+        model.addAttribute("products", productOptional);
+
+        return "admin/productsreg";
+    }
+    @PostMapping("product/active")
+    public String productActive(@RequestParam(name = "id") Integer id){
+
+        try {
+
+            productService.activeProduct(id);
+            return "redirect:/admin/productsreg";
+        }
+        catch(Exception ex){
+
+        }
+        return "/";
+
     }
 
 
@@ -116,6 +149,32 @@ public class ProductInController {
 
         }
         return "/";
+
+    }
+
+    @GetMapping("orders")
+    public String orders(Model model,  HttpServletRequest request){
+
+
+        model.addAttribute("orders", orderService.getAll());
+        return "/admin/orders";
+
+    }
+
+    @PostMapping("changeStatus")
+    public String changeStatus(@RequestParam(name = "status") OrderStatus status, @RequestParam(name = "id") Integer id){
+
+        orderService.updateStatus(status,id);
+        return "redirect:/admin/orders";
+    }
+
+    @GetMapping("orderdetail")
+    public String detail(@RequestParam(name="id") Integer id,Model model){
+
+        System.out.println("id =" + id);
+
+        model.addAttribute("productdetails", orderDetailService.getDetailByOrderId(id));
+        return "/admin/detail";
 
     }
 }
